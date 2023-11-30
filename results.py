1505cca0fc8e0b72e3554b9469e73aee51a136fd
jonasgrau <graujonas@gmail.com>
2023-11-30 15:26:21 +0100

    Commit

---
+++ results.py
@@ -0,0 +1,55 @@
+TechMetal, the optimal production plan,profit of 10354512.0 monetary units
+The number of employees in the period 1 is 250.0.
+The number of newly recruited Employees in period 1 150.0.
+The number of employees in the period 2 is 400.0.
+The number of newly recruited Employees in period 2 150.0.
+The number of employees in the period 3 is 502.0.
+The number of newly recruited Employees in period 3 102.0.
+The number of employees in the period 4 is 502.0.
+The number of employees in the period 5 is 296.0.
+The number of laid-off Employees in period 5 206.0.
+The number of employees in the period 6 is 295.0.
+The number of laid-off Employees in period 6 1.0.
+The number of employees in the period 7 is 295.0.
+The number of employees in the period 8 is 398.0.
+The number of newly recruited Employees in period 8 103.0.
+The number of employees in the period 9 is 409.0.
+The number of newly recruited Employees in period 9 11.0.
+The number of employees in the period 10 is 432.0.
+The number of newly recruited Employees in period 10 23.0.
+The number of employees in the period 11 is 477.0.
+The number of newly recruited Employees in period 11 45.0.
+The number of employees in the period 12 is 516.0.
+The number of newly recruited Employees in period 12 39.0.
+The costs for employee salaries amount to more than the planning period to 12598080.0 monetary units.
+The costs for overtime amount to more than the planning period to 301536.0 monetary units.
+The costs for new hires amount to over the planning period to 498400.0 monetary units.
+The costs for redundancies amount to more than the planning period to 207000.0 monetary units.
+The costs for warehousing amount to more than the planning period to 10600.0 monetary units.
+The costs for shortfalls amount to more than the planning period to 145536.0 monetary units.
+The costs for production material amount to more than the planning period to 8566240.0 monetary units.
+The costs for external procurement amount to over the planning period to 2793096.0 monetary units.
+The  month 1 average cost per unit is 55.48
+the procurement in month 1 is 26750.0
+The  month 2 average cost per unit is 53.55
+the procurement in month 2 is 14800.0
+The  month 3 average cost per unit is 53.66
+the procurement in month 3 is 138.0
+The  month 4 average cost per unit is 50.00
+the procurement in month 4 is 0.0
+The  month 5 average cost per unit is 60.64
+the procurement in month 5 is 0.0
+The  month 6 average cost per unit is 50.43
+the procurement in month 6 is 0.0
+The  month 7 average cost per unit is 50.01
+the procurement in month 7 is 0.0
+The  month 8 average cost per unit is 52.35
+the procurement in month 8 is 0.0
+The  month 9 average cost per unit is 50.24
+the procurement in month 9 is 0.0
+The  month 10 average cost per unit is 50.49
+the procurement in month 10 is 0.0
+The  month 11 average cost per unit is 50.86
+the procurement in month 11 is 0.0
+The  month 12 average cost per unit is 50.97
+the procurement in month 12 is 0.0
